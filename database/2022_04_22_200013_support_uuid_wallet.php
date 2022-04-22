8269fa9b72e3e63aa083a6b013b5c97eaa4ce1da
Maxim Babichev <maksim.babichev95@gmail.com>
2022-04-22 23:44:13 +0300

    2022_04_22_200013_support_uuid_wallet.php

--- database/2022_04_22_200013_support_uuid_wallet.php
+++ database/2022_04_22_200013_support_uuid_wallet.php
@@ -12,30 +12,33 @@ return new class() extends Migration {
     public function up(): void
     {
         Schema::table($this->transactionTable(), static function (Blueprint $table): void {
+            $table->dropIndex('payable_type_payable_id_ind');
+            $table->dropIndex('payable_type_ind');
+            $table->dropIndex('payable_confirmed_ind');
+            $table->dropIndex('payable_type_confirmed_ind');
+
+            $table->removeColumn('payable_id');
             $table->uuid('payable_id')
+                ->after('payable_type')
                 ->change()
             ;
-        });
 
-        Schema::table($this->walletTable(), function (Blueprint $table): void {
-            $table->uuid('holder_id')
-                ->change()
-            ;
-        });
-    }
-
-    public function down(): void
-    {
-        Schema::table($this->transactionTable(), static function (Blueprint $table): void {
-            $table->unsignedBigInteger('payable_id')
-                ->change()
-            ;
+            $table->index(['payable_type', 'payable_id'], 'payable_type_payable_id_ind');
+            $table->index(['payable_type', 'payable_id', 'type'], 'payable_type_ind');
+            $table->index(['payable_type', 'payable_id', 'confirmed'], 'payable_confirmed_ind');
+            $table->index(['payable_type', 'payable_id', 'type', 'confirmed'], 'payable_type_confirmed_ind');
         });
 
         Schema::table($this->walletTable(), static function (Blueprint $table): void {
-            $table->unsignedBigInteger('holder_id')
+            $table->dropUnique(['holder_type', 'holder_id', 'slug']);
+
+            $table->removeColumn('holder_id');
+            $table->uuid('holder_id')
+                ->after('holder_type')
                 ->change()
             ;
+
+            $table->unique(['holder_type', 'holder_id', 'slug']);
         });
     }
 
