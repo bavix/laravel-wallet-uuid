9ce7543fcc88cb40dbbd1e72cf603aa767135c13
Maxim Babichev <maksim.babichev95@gmail.com>
2022-04-23 00:28:11 +0300

    fix migration

--- database/2022_04_22_200013_support_uuid_wallet.php
+++ database/2022_04_22_200013_support_uuid_wallet.php
@@ -5,13 +5,19 @@ declare(strict_types=1);
 use Bavix\Wallet\Models\Transaction;
 use Bavix\Wallet\Models\Wallet;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class() extends Migration {
     public function up(): void
     {
-        Schema::dropColumns($this->transactionTable(), ['payable_id']);
-        Schema::dropColumns($this->walletTable(), ['holder_id']);
+        Schema::whenTableHasColumn($this->transactionTable(), 'payable_id', static function (Blueprint $table) {
+            $table->dropColumn('payable_id');
+        });
+
+        Schema::whenTableHasColumn($this->walletTable(), 'holder_id', static function (Blueprint $table) {
+            $table->dropColumn('holder_id');
+        });
     }
 
     private function transactionTable(): string
