c05b5f7789cdd2c7507820fa69f010cef10c6f7c
Maxim Babichev <maksim.babichev95@gmail.com>
2022-04-23 00:04:42 +0300

    test

--- database/2022_04_22_200013_support_uuid_wallet.php
+++ database/2022_04_22_200013_support_uuid_wallet.php
@@ -12,15 +12,10 @@ return new class() extends Migration {
     public function up(): void
     {
         Schema::table($this->transactionTable(), static function (Blueprint $table): void {
-            $table->dropIndex('payable_type_payable_id_ind');
-            $table->dropIndex('payable_type_ind');
-            $table->dropIndex('payable_confirmed_ind');
-            $table->dropIndex('payable_type_confirmed_ind');
             $table->dropColumn('payable_id');
         });
 
         Schema::table($this->walletTable(), static function (Blueprint $table): void {
-            $table->dropUnique(['holder_type', 'holder_id', 'slug']);
             $table->dropColumn('holder_id');
         });
     }
