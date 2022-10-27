f674f894a64274549ae2217e1f1dac6386686cf6
Maxim Babichev <maksim.babichev95@gmail.com>
2022-10-27 20:43:25 +0300

    rector parallel

--- rector.php
+++ rector.php
@@ -4,13 +4,13 @@ declare(strict_types=1);
 
 use Rector\CodeQuality\Rector\PropertyFetch\ExplicitMethodCallOverMagicGetSetRector;
 use Rector\Config\RectorConfig;
-use Rector\Core\Configuration\Option;
 use Rector\Laravel\Set\LaravelSetList;
 use Rector\Php74\Rector\Property\TypedPropertyRector;
 use Rector\PHPUnit\Set\PHPUnitSetList;
 use Rector\Set\ValueObject\SetList;
 
 return static function (RectorConfig $config): void {
+    $config->parallel();
     $config->paths([
         __DIR__ . '/src',
         __DIR__ . '/tests',
